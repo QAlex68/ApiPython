8837ec4fb9157381565911c74c510eb8082d7f8a
QAlex <qalex68@gmail.com>
2024-10-15 15:13:44 +0300

    Семинар03

---
+++ sem03/test_1.py
@@ -0,0 +1,48 @@
+import yaml, time
+from testpage import OperationsHelper
+import logging
+
+with open("./testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+name = testdata.get('username')
+passw = testdata.get('password')
+post_title = testdata.get('post_title')
+post_description = testdata.get('post_description')
+post_content = testdata.get('post_content')
+
+
+def test_step1(browser):
+    logging.info('Test 1 Starting')
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login('test')
+    testpage.enter_pass('test')
+    testpage.click_login_button()
+    assert testpage.get_error_text() == '401'
+
+
+def test_step2(browser):
+    logging.info("Test 2 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.enter_login(name)
+    testpage.enter_pass(passw)
+    testpage.click_login_button()
+    assert testpage.get_user_text() == f"Hello, {name}"
+
+# ДЗ02
+
+def test_add_post(browser):
+    logging.info("Test 3 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.click_create_post_button()
+    testpage.input_title_post(post_title)
+    testpage.input_description_post(post_description)
+    testpage.input_content_post(post_content)
+    testpage.click_save_post_button()
+    time.sleep(2)
+    assert testpage.get_new_title_text() == post_title
+
+
+if __name__ == '__main__':
+    pass
